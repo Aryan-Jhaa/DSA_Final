72f7bbf29ad6f161b35d208c98cb4586c1ce6578
aryan813 <aryanjha813@gmail.com>
2025-08-02 19:38:34 +0530

    Initial commit

---
+++ 1. python.py
@@ -0,0 +1,33 @@
+#variable 
+n = 10
+print(n)
+
+n = 'abc'
+print(n)
+
+#multiple assignment
+z,x = 10, 12
+print(z)
+print(x)
+
+#if 
+if z > 9:
+    print("Z is larger than 9")
+
+# parenthesis needed for multi-line conditions
+
+# and = &&
+# or = ||
+
+if((x>10 and x!=0) or (z<10)):
+    x += 1 
+    z=5
+print(x)
+print(z)
+
+#while
+
+a = 0
+while a<=5:
+    print(a)
+    a+=1
