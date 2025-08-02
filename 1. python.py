df17e74339510490facf7705306da153baf9fe56
aryan813 <aryanjha813@gmail.com>
2025-08-02 23:34:48 +0530

    Add 1st python

--- 1. python.py
+++ 1. python.py
@@ -73,3 +73,100 @@ print("2 power 3")
 print(math.pow(2, 3))
 print("sqrt of 2")
 print(math.sqrt(2))
+
+# Arrays, Lists in python:
+print("Array(lists) in python: ")
+arr = [1, 2, 3]
+print(arr)
+
+# can be used as an stack
+
+arr.append(5)
+print(arr)
+arr.pop()
+print(arr)
+
+# array inster (index, value):
+arr.insert(0, 10)
+print(arr)
+
+#sublists, slicing an array 
+print("slicing an array: ")
+print("Array before slicing: ")
+arr2 = [7, 8, 9, 10, 12, 15, 17, 18]
+print(arr2)
+print("After slicing: ")
+print(arr2[2:5])
+
+# loop through arrays:
+print("Printing array elements using for loop")
+for i in range (len(arr2)):
+    print(arr2[i])
+
+# Reversing an array:
+print("reversing an array: ")
+nums = [10, 8, 6, 4, 2, 0]
+print("Before reversing")
+print(nums)
+nums.reverse()
+print("After reversing: ")
+print(nums)
+
+# Sorting an array 
+sort_Array = [5, 1, 2, 10, 4, 6]
+print("Before sorting: ")
+print(sort_Array)
+
+sort_Array.sort()
+print("After sorting in ascending order: ")
+print(sort_Array)
+
+print("Sorting in descending order: ")
+sort_Array.sort(reverse=True)
+print(sort_Array)
+
+#Strings: 
+print("Strings: ")
+s = "abcdef"
+print(s[2:5])
+
+#Double ended Queue 
+from collections import deque
+
+que = deque()
+que.append(1)
+que.append(2)
+que.append(12)
+print(que)
+
+que.popleft()
+print(que)
+
+#Hash set:
+
+mySet = set()
+print("Hash sets: ")
+mySet.add(4)
+mySet.add(6)
+print(mySet)
+
+# Searching in hash set 
+print(1 in mySet)
+print(4 in mySet)
+
+#Hash Maps or Dictionaries
+city_map = {}
+
+citiesIn = ["Delhi", "Bangalore", "Mumbai"]
+city_map["India"] = []
+city_map["India"] += citiesIn
+citiesUS = ["New York", "Texas", "LA"]
+city_map["US"] = []
+city_map["US"] += citiesUS
+
+print(city_map)
+
+# Retrieving info from hash maps dot keys(), dot values(), dot items()
+
+city_map = city_map.values()
+print(city_map)
