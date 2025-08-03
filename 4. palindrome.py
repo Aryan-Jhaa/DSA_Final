154cc000018f48b324f77804bf0dd29ef54267d4
aryan813 <aryanjha813@gmail.com>
2025-08-03 15:54:54 +0530

    Add palindrome

---
+++ 4. palindrome.py
@@ -0,0 +1,19 @@
+n = input()
+
+num = int(n)
+result = 0
+
+while num>0:
+    ld = num%10
+    result = (result*10)+ld
+    num = num//10
+print("original num: " + str(n))
+
+print("result: " + str(result))
+
+if result == int(n):
+    print("It is a palindrome")
+
+else:
+    print("It is not a palindrome")
+    
